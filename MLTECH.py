17e5c8cab4796a0e60a2db5cc041d3a907e2a037
Sudhanshu Thakur <sudhanshuthakur8137@gmail.com>
2022-03-07 22:45:23 +0530

    Updated the scaling technique.

--- MLTECH.py
+++ MLTECH.py
@@ -11,10 +11,6 @@ y = dataset.iloc[:,11:].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0, shuffle = True)
 
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test= sc.transform(X_test)
 
 y_train = np.array(y_train)
 y_train = y_train.reshape(-1)
